911a5410e3d459a811a9ba1292adba95b2fee154
The King of Loser <r04943133@ntu.edu.tw>
2016-03-27 22:49:44 +0800

    Update counting.cu
    implement lab1/counting.cu

--- lab1/counting.cu
+++ lab1/counting.cu
@@ -11,53 +11,66 @@
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
-__global__ void part1_init(const char *, int *, int);
-__global__ void part1_kernel(const char *, int *, int, int);
-void CountPosition(const char *text, int *pos, int text_size)
+__global__ void init(const char *, int *, int);
+__global__ void kernel(const char *, int *, int, int);
+
+void CountPosition(const char *text, int *pos, int text_size)//part1
 {
-	const int maxK = 500;
-	const int threadCount = 512;
-	int blockCount = text_size / threadCount + 1;
-	part1_init<<<blockCount, threadCount>>>(text, pos, text_size);
+	const int k = 500;
+	const int blocksize = 512;
+	int nblock = text_size / blocksize + (text_size % blocksize == 0?0:1);
+	part1_init<<<nblock, blocksize>>>(text, pos, text_size);
 	cudaDeviceSynchronize();
-    for (int i = 2; i <= maxK; ++i) {
-      blockCount = (text_size / i + 1) / threadCount + 1;
-      part1_kernel<<<blockCount, threadCount>>>(text, pos, text_size, i);
+    for (int i = 2; i <= k; i++) 
+	{
+      nblock = (text_size / i + 1) / blocksize + (text_size % blocksize == 0?0:1);
+      part1_kernel<<<nblock, blocksize>>>(text, pos, text_size, i);
       cudaDeviceSynchronize();
     }
 }
 
-__global__ void part1_kernel(const char * text, int * pos, int n, int k) {
-   int id = blockIdx.x * blockDim.x + threadIdx.x;
-   id = id * k;
-   /* should be careful here as id may int overflow */
-   if (id >= n) {
+ __global__ void init(const char * text, int * pos, int n) //using time complexity nlogk algorithm
+ {
+   int idx = blockIdx.x * blockDim.x + threadIdx.x;
+   if (idx >= n) //out of boundry
+   {
      return;
    }
-   if (pos[id] == 0) {
+   else
+   {
+	if (text[idx] == '\n') //white space output 0
+	{
+		pos[idx] = 0;
+	} 
+	else //not white space output 1
+	{
+		pos[idx] = 1;
+	}
+   }
+ }
+__global__ void kernel(const char * text, int * pos, int n, int k) 
+{
+   int idx = blockIdx.x * blockDim.x + threadIdx.x;
+   int idx = idx * k;
+   if (idx >= n) {
      return;
    }
-   if (id > 0 && pos[id-1] == k-1) {
-     pos[id] = k;
+   if (pos[idx] == 0) {
      return;
    }
-   int i = pos[id];
-   int j = id + (k - i - 1);
-   if (j+1 < n && pos[j] == k-1 && text[j+1] != '\n') {
+   if (idx > 0 && pos[idx-1] == k-1) {
+     pos[idx] = k;
+     return;
+   }
+   int i = pos[idx];
+   int j = idx + (k - i - 1);
+   if (j+1 < n && pos[j] == k-1 && text[j+1] != '\n') 
+   {
      pos[j+1] = k;
    }
  }
 
- __global__ void part1_init(const char * text, int * pos, int n) {
-   int id = blockIdx.x * blockDim.x + threadIdx.x;
-   if (id >= n) {
-     return;
-  }
-   if (text[id] == '\n') {
-     pos[id] = 0;
-   } else {
-     pos[id] = 1;
-   }
+
  
 class isOne
 {
@@ -68,7 +81,7 @@ class isOne
 	}
 }
 
-int ExtractHead(const int *pos, int *head, int text_size)
+int ExtractHead(const int *pos, int *head, int text_size)//part2
 {
 	int *buffer;
 	int nhead;
@@ -77,12 +90,42 @@ int ExtractHead(const int *pos, int *head, int text_size)
 	thrust::device_ptr<int> head_d(head), flag_d(buffer), cumsum_d(buffer+text_size);
 
 	// TODO
-	auto head_end_d = thrust::copy_if(thrust::counting_iterator<int>(0), thrust::countung_iterator<int>(text_size), pos_d, head_d, isOne());
+	int head_end_d = thrust::copy_if(thrust::counting_iterator<int>(0), thrust::countung_iterator<int>(text_size), pos_d, head_d, isOne());
 	nhead = head_end_d - head_d;
 	cudaFree(buffer);
 	return nhead;
 }
 
+//Convert all characters to be capital
+__global__ void Transform_1(char *input_gpu, int fsize) 
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < fsize and 97 <= input_gpu[idx] and input_gpu[idx] <= 122) 
+	{
+		input_gpu[idx] = input_gpu[idx] - 32;
+	}
+}
+
+//Swap all pairs in all words
+__global__ void Transform_2(char *input_gpu, int fsize) 
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	char temp;
+	if (idx < fsize and input_gpu[idx] != '\n' and input_gpu[idx+1] != '\n') 
+	{
+		temp = input_gpu[idx];
+		input_gpu[idx] = input_gpu[idx+1];
+		input_gpu[idx+1] = temp;
+		idx = idx + 2;
+	}
+	else if(idx < fsize and input_gpu[idx] != '\n' and input_gpu[idx+1] == '\n')
+	{
+		input_gpu[idx] = input_gpu[idx];
+	}
+}
+
 void Part3(char *text, int *pos, int *head, int text_size, int n_head)
 {
+	Transform_1<<<8, 32>>>(text, text_size);
+	Transform_2<<<8, 32>>>(text, text_size);
 }
