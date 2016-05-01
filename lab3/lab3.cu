31e94f3ac4c4473b5e4fcdb6de3cab6ff0402b92
The King of Loser <r04943133@ntu.edu.tw>
2016-05-02 02:40:36 +0800

    Update lab3.cu
    plus some commit
    debug

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -1,8 +1,6 @@
 #include "lab3.h"
 #include <cstdio>
-#include <iostream>
-
-using namespace std;
+#include <Timer.h>
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
@@ -13,82 +11,97 @@ __global__ void CalculateFixed(
 	const float *target,
 	const float *mask,
 	float *output,
-	const int wb, const int hb, const int wt, const int ht,
+	const int wb, const int hb, 
+	const int wt, const int ht,
 	const int oy, const int ox	
 )
 {
 	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
 	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
-	const int curt = wt*yt+xt;//coordinate
-	if (yt < ht and xt < wt and mask[curt] > 127.0f) {//to simple clone
-		return;
+
+	//target array index for the current pixel location
+	const int curt = wt*yt + xt;//coordinate
+	
+	if(0 <= xt && xt < wt && 0 <= yt && yt < ht && mask[curt] > 127.0f)
+	{
+		float Nb, Wb, Sb, Eb, Nt, Wt, St, Et;
+		const int yb = oy+yt, xb = ox+xt;
+		// background array index for the current pixel location
+		const int curb = wb*yb+xb;
+		if (0 <= yb && yb < hb && 0 <= xb && xb < wb) 
+		{
+			for(int i = 0; i < 3; i++) 
+			{
+				Nb = (yb > 0) ? ((yt == 0 || mask[curt-wt] < 127.0f) ? background[(curb-wb)*3+i]:0) :background[curb*3+i];
+				Wb = (xb > 0) ? ((xt == 0 || mask[curt-1] < 127.0f) ? background[(curb-1)*3+i]:0) :background[curb*3+i];
+				Sb = (yb < hb-1) ? ((yt == ht-1 || mask[curt+wt] < 127.0f) ? background[(curb+wb)*3+i]:0) :background[curb*3+i];
+				Eb = (xb < wb-1) ? ((xt == wt-1 || mask[curt+1] < 127.0f) ? background[(curb+1)*3+i]:0) :background[curb*3+i];
+				Nt = (yt > 0) ? target[(curt-wt)*3+i] :target[curt*3+i];
+				Wt = (xt > 0) ? target[(curt-1)*3+i] :target[curt*3+i];
+				St = (yt < ht-1) ? target[(curt+wt)*3+i] :target[curt*3+i];
+				Et = (xt < wt-1) ? target[(curt+1)*3+i] :target[curt*3+i];
+		
+				output[curt*3+i] = 4*target[curt*3+i] - (Nt + Wt + St + Et) + (Nb + Wb + Sb+ Eb);
+			}
+		}
 	}
-	const int dir[4][2]{{1,1}, {1,-1}, {-1,1}, {-1,-1}};//definition direction
-	float temp[3];
-	temp[0] = 4*target[3*curt+0];
-	temp[1] = 4*target[3*curt+1];
-	temp[2] = 4*target[3*curt+2];
-	for(int i = 0;i < 4;i++)
-	{	//declare neighbor coordinate
-		const int nx = idx + dir[i][0];
-		const int ny = idy + dir[i][0];
-		const int curn = wt*ny + nx;
-		if (nx >= 0 && ny >= 0 && nx < wt && ny < ht) {//inside boundry
-       temp[0] -= target[3*curn + 0];
-       temp[1] -= target[3*curn + 1];
-       temp[2] -= target[3*curn + 2];
-     } else {
-       temp[0] -= 255.0f;
-       temp[1] -= 255.0f;
-       temp[2] -= 255.0f;
-     }
-     if ((nx < 0 || ny < 0 || nx >= wt || ny >= ht) || mask[curn] < 127.0f) {//at edge
-		const int bx = nx + ox;
-		const int by = ny + oy;
-		const int curb = (wb*by + bx)*3;
-		temp[0] += background[curb+0];
-		temp[1] += background[curb+1];
-		temp[2] += background[curb+2];
-     }
-   }
-	output[3*curt+0] = temp[0];
-	output[3*curt+1] = temp[1];
-	output[3*curt+2] = temp[2];
+	else
+	{
+		output[curt*3] = 0.0f;
+		output[curt*3+1] = 0.0f;
+		output[curt*3+2] = 0.0f;
+	}	
 }
 
 __global__ void PoissonImageCloningIteration(
 	const float *fixed,
-	const float *target,
 	const float *mask,
+	const float *target,
 	float *output,
-	const int wb, const int hb, const int wt, const int ht,
-	const int oy, const int ox
+	const int wt, 
+	const int ht
 )
 {
-	const int idx = blockDim.x * blockIdx.x + threadIdx.x;
-	const int idy = blockDim.y * blockIdx.y + threadIdx.y;
-	const int curt = wt * idy + idx;
-	if (idx >= wt || idy >= ht || mask[curt] < 127.0f) {
-		return;
+	const int xt = blockDim.x * blockIdx.x + threadIdx.x;
+	const int yt = blockDim.y * blockIdx.y + threadIdx.y;
+	const int curt = wt*yt + xt;
+	float Nb, Wb, Sb, Eb;
+	
+	if(0 <= xt && xt < wt && 0 <= yt && yt < ht && mask[curt] > 127.0f)//inside boundry
+	{
+		for(int i = 0; i < 3; i++) 
+		{
+			Nb = (yt == 0 || mask[curt-wt] < 127.0f) ?0 :target[(curt-wt)*3+i];
+			Wb = (xt == 0 || mask[curt-1] < 127.0f) ?0 :target[(curt-1)*3+i];
+			Sb = (yt == ht-1 || mask[curt+wt] < 127.0f) ?0 :target[(curt+wt)*3+i];
+			Eb = (xt == wt-1 || mask[curt+1] < 127.0f) ?0 :target[(curt+1)*3+i];
+			output[curt*3+i] = (fixed[curt*3+i] + Nb + Wb + Sb + Eb)/4;
+		}
 	}
-	float temp[3];
-   temp[0] = fixed[3*curt+0];
-   temp[1] = fixed[3*curt+1];
-   temp[2] = fixed[3*curt+2];
-   const int dir[4][2] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-   for (int i = 0; i < 4; ++i) {
-     const int nx = idx + dir[i][0];
-     const int ny = idy + dir[i][1];
-     const int curn = wt*ny + nx;
-     if (nx >= 0 && nx < wt && ny >= 0 && ny < ht && mask[curn] > 127.0f) {
-       temp[0] += target[3*curn+0];
-       temp[1] += target[3*curn+1];
-       temp[2] += target[3*curn+2];
-     }
-   }
-   output[3*curt+0] = temp[0] / 4;
-   output[3*curt+1] = temp[1] / 4;
-   output[3*curt+2] = temp[2] / 4;
+	else
+	{
+		output[curt*3] = target[curt*3];
+		output[curt*3+1] = target[curt*3+1];
+		output[curt*3+2] = target[curt*3+2];
+	}
+}
+
+//Implement  successive over-relaxation acceleration
+__global__ void sor(
+	float w,
+	float *cur,
+	float *nxt,
+	const int wt,
+	const int ht 
+)
+{
+	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
+	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+	const int curt = wt*yt + xt;
+	
+	nxt[curt*3] = nxt[curt*3] + (1-w)*cur[curt*3];
+	nxt[curt*3+1] = nxt[curt*3+1] + (1-w)*cur[curt*3+1];
+	nxt[curt*3+2] = nxt[curt*3+2] + (1-w)*cur[curt*3+1];
 }
 
 __global__ void SimpleClone(
@@ -96,16 +109,18 @@ __global__ void SimpleClone(
 	const float *target,
 	const float *mask,
 	float *output,
-	const int wb, const int hb, const int wt, const int ht,
+	const int wb, const int hb, 
+	const int wt, const int ht,
 	const int oy, const int ox
 )
 {
-	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
 	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
-	const int curt = wt*yt+xt;
+	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+	const int curt = wt*yt + xt;
+
 	if (yt < ht and xt < wt and mask[curt] > 127.0f) {
-		const int yb = oy+yt, xb = ox+xt;
-		const int curb = wb*yb+xb;
+		const int yb = oy + yt, xb = ox + xt;
+		const int curb = wb*yb + xb;
 		if (0 <= yb and yb < hb and 0 <= xb and xb < wb) {
 			output[curb*3+0] = target[curt*3+0];
 			output[curb*3+1] = target[curt*3+1];
@@ -128,23 +143,57 @@ void PoissonImageCloning(
 	cudaMalloc(&fixed, 3*wt*ht*sizeof(float));
 	cudaMalloc(&buf1, 3*wt*ht*sizeof(float));
 	cudaMalloc(&buf2, 3*wt*ht*sizeof(float));
-	
+
+	//timer start	
+	cudaDeviceSynchronize();
+	Timer timer;
+	timer.Start();
+
 	// initialize the iteration
+	//Gridsize: (wt/32 + wt%32) * (ht/16 + ht%16) blocks; Blocksize: 32*16 threads
+	//dim3 is a 3D structure, flatten to 2D for simpler usage here
 	dim3 gdim(CeilDiv(wt,32), CeilDiv(ht,16)), bdim(32,16);
 	CalculateFixed<<<gdim, bdim>>>(
 	background, target, mask, fixed,
 	wb, hb, wt, ht, oy, ox
 	);
+	cudaDeviceSynchronize();
 	cudaMemcpy(buf1, target, sizeof(float)*3*wt*ht, cudaMemcpyDeviceToDevice);
+	for (int i = 0; i < 10; i++)
+	{
+		// use buf1 as reference and write to buf2
+		PoissonImageCloningIteration<<<gdim, bdim>>>(
+			fixed, mask, buf1, buf2, wt, ht
+		);
+		cudaDeviceSynchronize();
+		//run SOR
+		sor<<<gdim, bdim>>>(w, buf1, buf2, wt, ht);
+		cudaDeviceSynchronize();
+		
+		// use buf2 as reference and write back to buf1
+		PoissonImageCloningIteration<<<gdim, bdim>>>(
+			fixed, mask, buf2, buf1, wt, ht
+		);
+		cudaDeviceSynchronize();
+		
+		//run SOR
+		sor<<<gdim, bdim>>>(w, buf2, buf1, wt, ht);
+		cudaDeviceSynchronize();
+	}
 	
 	// iterate
-	for (int i = 0; i < 10000; ++i) {
+	for (int i = 0; i < 5000; ++i) {
+	//use buf1 as reference and write to buf2
 	PoissonImageCloningIteration<<<gdim, bdim>>>(
 	fixed, mask, buf1, buf2, wt, ht
 	);
+	cudaDeviceSynchronize();
+
+	//use buf2 as reference and write to buf1
 	PoissonImageCloningIteration<<<gdim, bdim>>>(
 	fixed, mask, buf2, buf1, wt, ht
 	);
+	cudaDeviceSynchronize();
 	}
 	
 	// copy the image back
@@ -152,7 +201,12 @@ void PoissonImageCloning(
 	SimpleClone<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
 		background, target, mask, output,
 		wb, hb, wt, ht, oy, ox
-	);
+	);	
+	cudaDeviceSynchronize();
+	
+	//print timer
+	timer.Pause();
+	printf_timer(timer);
 	
 	// clean up
 	cudaFree(fixed);
